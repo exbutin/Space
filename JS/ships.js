40c6517aee2b4b1aa92b8e2b77b23e49698242e3
exbutin <108779423+exbutin@users.noreply.github.com>
2022-07-05 22:56:30 -0500

    Space Age V1

---
+++ JS/ships.js
@@ -0,0 +1,151 @@
+// Query
+"use strict";
+// Arrays
+//all
+const tip = [
+  "/Images/Ship Cards DARK_01.jpg",
+  "/Images/Ship Cards DARK_02.jpg",
+  "/Images/Ship Cards DARK_03.jpg",
+  "/Images/Ship Cards DARK_04.jpg",
+];
+
+const adapter = [
+  "/Images/Ship Cards DARK_17.jpg",
+  "/Images/Ship Cards DARK_18.jpg",
+  "/Images/Ship Cards DARK_19.jpg",
+  "/Images/Ship Cards DARK_20.jpg",
+];
+
+const engine = [
+  "/Images/Ship Cards DARK_05.jpg",
+  "/Images/Ship Cards DARK_06.jpg",
+  "/Images/Ship Cards DARK_07.jpg",
+  "/Images/Ship Cards DARK_08.jpg",
+];
+
+const mid = [
+  "/Images/Ship Cards DARK_09.jpg",
+  "/Images/Ship Cards DARK_10.jpg",
+  "/Images/Ship Cards DARK_11.jpg",
+  "/Images/Ship Cards DARK_12.jpg",
+  "/Images/Ship Cards DARK_13.jpg",
+  "/Images/Ship Cards DARK_14.jpg",
+  "/Images/Ship Cards DARK_15.jpg",
+  "/Images/Ship Cards DARK_16.jpg",
+];
+
+//large
+const large_tip = [
+  "/Images/Ship Cards DARK_03.jpg",
+  "/Images/Ship Cards DARK_04.jpg",
+];
+
+const large_engine = [
+  "/Images/Ship Cards DARK_05.jpg",
+  "/Images/Ship Cards DARK_06.jpg",
+];
+
+const large_mid = [
+  "/Images/Ship Cards DARK_13.jpg",
+  "/Images/Ship Cards DARK_14.jpg",
+  "/Images/Ship Cards DARK_15.jpg",
+  "/Images/Ship Cards DARK_16.jpg",
+];
+
+//small
+const small_tip = [
+  "/Images/Ship Cards DARK_01.jpg",
+  "/Images/Ship Cards DARK_02.jpg",
+];
+
+const small_engine = [
+  "/Images/Ship Cards DARK_07.jpg",
+  "/Images/Ship Cards DARK_08.jpg",
+];
+
+const small_mid = [
+  "/Images/Ship Cards DARK_09.jpg",
+  "/Images/Ship Cards DARK_10.jpg",
+  "/Images/Ship Cards DARK_11.jpg",
+  "/Images/Ship Cards DARK_12.jpg",
+];
+//Functions
+function shuffle(array) {
+  let currentIndex = array.length,
+    randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex != 0) {
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+
+  return array;
+}
+
+function true_random() {
+  shuffle(tip);
+  shuffle(adapter);
+  shuffle(mid);
+  shuffle(engine);
+
+  //tips
+  let tip_1 = tip[0];
+  let tip_2 = tip[1];
+  document.querySelector("#tip_1").src = tip_1;
+  document.querySelector("#tip_2").src = tip_2;
+
+  // document.querySelector("#tip_1").src = tip[0];
+  // document.querySelector("#tip_2").src = tip[1];
+
+  //adapter
+  document.querySelector("#adapter_1").src = adapter[0];
+  document.querySelector("#adapter_2").src = adapter[1];
+
+  //mid
+  document.querySelector("#mid_1").src = mid[0];
+  document.querySelector("#mid_2").src = mid[1];
+  document.querySelector("#mid_3").src = mid[2];
+  document.querySelector("#mid_4").src = mid[3];
+
+  //engine
+  document.querySelector("#engine_1").src = engine[0];
+  document.querySelector("#engine_2").src = engine[1];
+}
+
+function distributed_random() {
+  shuffle(large_tip);
+  shuffle(large_adapter);
+  shuffle(large_mid);
+  shuffle(large_engine);
+
+  shuffle(small_tip);
+  shuffle(small_adapter);
+  shuffle(small_mid);
+  shuffle(small_engine);
+
+  //tips
+  document.querySelector("#tip_1").src = large_tip[0];
+  document.querySelector("#tip_2").src = small_tip[0];
+
+  //adapter
+  document.querySelector("#adapter_1").src = large_adapter[0];
+  document.querySelector("#adapter_2").src = small_adapter[0];
+
+  //mid
+  document.querySelector("#mid_1").src = large_mid[0];
+  document.querySelector("#mid_2").src = small_mid[0];
+  document.querySelector("#mid_3").src = large_mid[1];
+  document.querySelector("#mid_4").src = small_mid[1];
+
+  //engine
+  document.querySelector("#engine_1").src = large_engine[0];
+  document.querySelector("#engine_2").src = small_engine[0];
+}
---
+++ JS/turly_random.js
@@ -0,0 +1,195 @@
+// Query
+// Arrays
+//all
+
+//Functions
+function shuffle(array) {
+  let currentIndex = array.length,
+    randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex != 0) {
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+
+  return array;
+}
+
+function intialize() {
+  //set array
+  const tip = [
+    "/Images/Ship Cards DARK_01.jpg",
+    "/Images/Ship Cards DARK_02.jpg",
+    "/Images/Ship Cards DARK_03.jpg",
+    "/Images/Ship Cards DARK_04.jpg",
+  ];
+
+  const adapter = [
+    "/Images/Ship Cards DARK_17.jpg",
+    "/Images/Ship Cards DARK_18.jpg",
+    "/Images/Ship Cards DARK_19.jpg",
+    "/Images/Ship Cards DARK_20.jpg",
+  ];
+
+  const engine = [
+    "/Images/Ship Cards DARK_05.jpg",
+    "/Images/Ship Cards DARK_06.jpg",
+    "/Images/Ship Cards DARK_07.jpg",
+    "/Images/Ship Cards DARK_08.jpg",
+  ];
+
+  const mid = [
+    "/Images/Ship Cards DARK_09.jpg",
+    "/Images/Ship Cards DARK_10.jpg",
+    "/Images/Ship Cards DARK_11.jpg",
+    "/Images/Ship Cards DARK_12.jpg",
+    "/Images/Ship Cards DARK_13.jpg",
+    "/Images/Ship Cards DARK_14.jpg",
+    "/Images/Ship Cards DARK_15.jpg",
+    "/Images/Ship Cards DARK_16.jpg",
+  ];
+
+  //large
+  const large_tip = [
+    "/Images/Ship Cards DARK_03.jpg",
+    "/Images/Ship Cards DARK_04.jpg",
+  ];
+
+  const large_engine = [
+    "/Images/Ship Cards DARK_05.jpg",
+    "/Images/Ship Cards DARK_06.jpg",
+  ];
+
+  const large_mid = [
+    "/Images/Ship Cards DARK_13.jpg",
+    "/Images/Ship Cards DARK_14.jpg",
+    "/Images/Ship Cards DARK_15.jpg",
+    "/Images/Ship Cards DARK_16.jpg",
+  ];
+
+  //small
+  const small_tip = [
+    "/Images/Ship Cards DARK_01.jpg",
+    "/Images/Ship Cards DARK_02.jpg",
+  ];
+
+  const small_engine = [
+    "/Images/Ship Cards DARK_07.jpg",
+    "/Images/Ship Cards DARK_08.jpg",
+  ];
+
+  const small_mid = [
+    "/Images/Ship Cards DARK_09.jpg",
+    "/Images/Ship Cards DARK_10.jpg",
+    "/Images/Ship Cards DARK_11.jpg",
+    "/Images/Ship Cards DARK_12.jpg",
+  ];
+
+  //add to local
+  localStorage.setItem("tip_array_base", JSON.stringify(tip));
+  localStorage.setItem("adapter_array_base", JSON.stringify(adapter));
+  localStorage.setItem("mid_array_base", JSON.stringify(mid));
+  localStorage.setItem("engine_array_base", JSON.stringify(engine));
+
+  localStorage.setItem("large_tip_array", JSON.stringify(large_tip));
+  localStorage.setItem("large_mid_array", JSON.stringify(large_mid));
+  localStorage.setItem("large_engine_array", JSON.stringify(large_engine));
+
+  localStorage.setItem("small_tip_array", JSON.stringify(small_tip));
+  localStorage.setItem("small_mid_array", JSON.stringify(small_mid));
+  localStorage.setItem("small_engine_array", JSON.stringify(small_engine));
+}
+
+function true_random() {
+  localStorage.setItem(
+    "tip_array",
+    JSON.stringify(shuffle(JSON.parse(localStorage.getItem("tip_array_base"))))
+  );
+  localStorage.setItem(
+    "adapter_array",
+    JSON.stringify(
+      shuffle(JSON.parse(localStorage.getItem("adapter_array_base")))
+    )
+  );
+  localStorage.setItem(
+    "mid_array",
+    JSON.stringify(shuffle(JSON.parse(localStorage.getItem("mid_array_base"))))
+  );
+  localStorage.setItem(
+    "engine_array",
+    JSON.stringify(
+      shuffle(JSON.parse(localStorage.getItem("engine_array_base")))
+    )
+  );
+}
+
+function distributed_random() {
+  let adapter = shuffle(JSON.parse(localStorage.getItem("adapter_array_base")));
+  let large_tip = shuffle(JSON.parse(localStorage.getItem("large_tip_array")));
+  let large_mid = shuffle(JSON.parse(localStorage.getItem("large_mid_array")));
+  let large_engine = shuffle(
+    JSON.parse(localStorage.getItem("large_engine_array"))
+  );
+  let small_tip = shuffle(JSON.parse(localStorage.getItem("small_tip_array")));
+  let small_mid = shuffle(JSON.parse(localStorage.getItem("small_mid_array")));
+  let small_engine = shuffle(
+    JSON.parse(localStorage.getItem("small_engine_array"))
+  );
+
+  let tip = [large_tip[0], small_tip[0]];
+  let mid = [small_mid[0], large_mid[0], small_mid[1], large_mid[1]];
+  let engine = [small_engine[0], large_engine[0]];
+
+  localStorage.setItem("tip_array", JSON.stringify(tip));
+  localStorage.setItem("adapter_array", JSON.stringify(adapter));
+  localStorage.setItem("engine_array", JSON.stringify(engine));
+  localStorage.setItem("mid_array", JSON.stringify(mid));
+}
+
+function update_ships() {
+  //tips
+  document.querySelector("#tip_1").src = JSON.parse(
+    localStorage.getItem("tip_array")
+  )[0];
+  document.querySelector("#tip_2").src = JSON.parse(
+    localStorage.getItem("tip_array")
+  )[1];
+
+  //adapter
+  document.querySelector("#adapter_1").src = JSON.parse(
+    localStorage.getItem("adapter_array")
+  )[0];
+  document.querySelector("#adapter_2").src = JSON.parse(
+    localStorage.getItem("adapter_array")
+  )[1];
+
+  //mid
+  document.querySelector("#mid_1").src = JSON.parse(
+    localStorage.getItem("mid_array")
+  )[0];
+  document.querySelector("#mid_2").src = JSON.parse(
+    localStorage.getItem("mid_array")
+  )[1];
+  document.querySelector("#mid_3").src = JSON.parse(
+    localStorage.getItem("mid_array")
+  )[2];
+  document.querySelector("#mid_4").src = JSON.parse(
+    localStorage.getItem("mid_array")
+  )[3];
+
+  //engine
+  document.querySelector("#engine_1").src = JSON.parse(
+    localStorage.getItem("engine_array")
+  )[0];
+  document.querySelector("#engine_2").src = JSON.parse(
+    localStorage.getItem("engine_array")
+  )[1];
+}
